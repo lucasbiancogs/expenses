43b76d39e907ca671847cde1d64ef2dcd3fcf639
lucasbiancogs <lucas.biancogs@hotmail.com>
2020-07-10 01:04:02 -0300

    Adding models - transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,13 +13,42 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 310.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.31,
+      date: DateTime.now()
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas pessoais'),
       ),
-      body: Center(child: Text('Versão inicial'),),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Gráfico'),
+              elevation: 5,
+              color: Colors.purple,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      ),
     );
   }
 }
